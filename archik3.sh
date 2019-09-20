70f413cae0d127fff5c48fe7c7171f74b3dac50c
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-09-20 20:14:23 +0300

    Update archik3.sh

--- archik3.sh
+++ archik3.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+echo 'Добавляем пользователя и ставим ему пароль'
+useradd -m -g users -G wheel -s /bin/bash sergey
+(
+    echo 1998;
+    echo 1998;
+)   | passwd sergey
+
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+
 echo 'Установка основных программ'
 #Установка xorg
 sudo pacman -S xorg-server xorg-xinit xorg-xbacklight 
