a259aae8041c679de5c582d80036b0dce95b9536
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-09-20 22:42:01 +0300

    Update archik3.sh

--- archik3.sh
+++ archik3.sh
@@ -17,45 +17,26 @@ pacman -Syy
 
 echo 'Установка основных программ'
 #Установка xorg
-sudo pacman -S xorg-server xorg-xinit xorg-xbacklight 
+pacman -S xorg-server xorg-xinit xorg-xbacklight 
 
 #Установка экранного менеджера
-sudo pacman -S i3-wm i3status i3lock rofi firefox firefox-i18n-ru rxvt-unicode thunar vim feh lxappearance compton 
+pacman -S i3-wm i3status i3lock rofi firefox firefox-i18n-ru rxvt-unicode thunar vim feh lxappearance compton 
 
 #Установка экрана входа
-sudo pacman -S lightdm lightdm-gtk-greeter 
+pacman -S lightdm lightdm-gtk-greeter 
 
 #Установка драйверов
-sudo pacman -S xf86-video-intel lib32-mesa 
+pacman -S xf86-video-intel lib32-mesa 
 
 #Драйвера на Тачпад
-sudo pacman -S xf86-input-synaptics 
+pacman -S xf86-input-synaptics 
 
 #Драйвера на Звук
-sudo pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils alsa-oss 
+pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils alsa-oss 
 
 #Шрифты
-sudo pacman -S ttf-liberation ttf-droid ttf-dejavu 
-
-#Bluetooth
-sudo pacman -S blueman blueman-manager-applet 
+pacman -S ttf-liberation ttf-droid ttf-dejavu 
 
 #Сетевая утилита
-sudo pacman -S networkmanager network-manager-applet 
-
-#Configs
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo "exec i3" >> ~/.xinitrc 
-
-mkdir -p ~/.config/i3/config
-
-mkdir -p ~/.config/i3status/config
-
-cp /etc/i3/config ~/.config/i3/config
-
-cp /etc/i3status.conf ~/.config/i3status/config
-
-
-#Активация i3
-systemctl enable NetworkManager lightdm.service
+pacman -S networkmanager network-manager-applet 
 
