8a58e092eed846ad90c184453c1005092259dc11
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-09-23 18:16:37 +0300

    Update archik2.sh

--- archik2.sh
+++ archik2.sh
@@ -48,6 +48,50 @@ echo 'passwd'
     echo 1998;
 )   | passwd 
 
-umount -R /mnt
-reboot
-exit
+echo 'Добавляем пользователя и ставим ему пароль'
+useradd -m -g users -G wheel -s /bin/bash sergey
+(
+    echo 1998;
+    echo 1998;
+)   | passwd sergey
+
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+echo 'Установка основных программ'
+echo 'Установка xorg'
+pacman -S xorg-server xorg-apps xorg-xinit --noconfirm --needed
+
+echo 'Установка экранного менеджера'
+pacman -S i3-wm i3status i3lock dmenu  --noconfirm --needed
+
+echo 'Установка экрана входа'
+pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
+
+echo 'Сетевая утилита'
+pacman -S networkmanager network-manager-applet networkmanager-openconnect --noconfirm --needed
+
+echo 'Включение менеджера входа и сетевой утилиты'
+systemctl enable lightdm NetworkManager
+
+echo 'Ставим загрузку i3'
+echo "exec i3" >> ~/.xinitrc
+
+echo 'Установка драйверов, шрифтов, программ'
+echo 'Установка драйверов'
+pacman -S xf86-video-intel lib32-intel-dri xf86-input-synaptics --noconfirm --needed 
+
+
+echo 'Драйвера на Звук'
+pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils alsa-oss --noconfirm --needed
+
+echo 'Шрифты'
+pacman -S ttf-liberation ttf-droid ttf-dejavu --noconfirm --needed
+
+echo 'Программы'
+pacman -S rxvt-unicode vim pcmanfm pcmanfm-gtk3 gvfs udisks firefox firefox-i18n-ru feh lxappaerance tar p7zip file-roller  --noconfirm --needed
