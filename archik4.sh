f5db28d963a101a2af4a0bcaab932992a6c2f3fd
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-09-24 00:39:45 +0300

    Update archik4.sh

--- archik4.sh
+++ archik4.sh
@@ -7,25 +7,37 @@ mkdir ~/Video
 mkdir ~/.icons
 mkdir ~/.themes
 
-mkdir -p ~/.config/i3status.config
+mkdir -p ~/.config/i3status/config
 
 cp /etc/i3status.conf ~/.config/i3status/config
 
 echo 'Установка утилит'
-(
-    echo 1998;
-)   | sudo pacman -S pcmanfm gvfs tar p7zip file-roller feh lxappearance unrar dosfstools ntfs-3g pulseaudio pulseaudio-bluetooth pulseaudio-alsa  alsa-lib alsa-utils --noconfirm --needed
+sudo pacman -S  gvfs tar p7zip file-roller feh lxappearance unrar dosfstools ntfs-3g pulseaudio pulseaudio-bluetooth pulseaudio-alsa  alsa-lib alsa-utils --noconfirm --needed
 
 echo 'Установка программ'
-sudo pacman -S firefox firefox-i18n-ru libreoffice-fresh libre-office-fresh-ru libreffice-fresh-uk gnome-calculator vlc lxmusic gpicview evince vim compton screenfetch ufw qbittorrent kdeconnect git skype wget --noconfirm --needed
+sudo pacman -S  libreoffice-fresh libreoffice-fresh-ru libreoffice-fresh-uk gnome-calculator vlc lxmusic gpicview evince vim compton screenfetch ufw qbittorrent kdeconnect  --noconfirm --needed
+
+sudo pacman -Syyu 
+
+cd Downloads
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si --needed --noconfirm --skippgpcheck
+cd ~
 
 echo 'Установка yay'
-cd /Downloads
+cd Downloads
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -sir --needed --noconfirm --skippgpcheck
+makepkg -si --needed --noconfirm --skippgpcheck
+cd ~
+
+cd Downloads
+git clone https://aur.archlinux.org/timeshift.git
+cd timeshift
+makepkg -si --needed --noconfirm --skippgpcheck
+cd ~
 
 echo 'Установка программ из AUR'
-(
-    echo 1998;
-)   | yay -S timeshift xkblayout-state onedrive --needed --noconfirm --skippgpcheck
+
+yay -S  xkblayout-state onedrive --needed --noconfirm 
