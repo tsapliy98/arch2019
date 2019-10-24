ed9dda8eaaff846de227b1aa5b76bd828770136a
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-25 01:35:43 +0300

    Update archik.sh

--- archik.sh
+++ archik.sh
@@ -9,27 +9,33 @@ setfont cyr-sun16
 echo 'Синхронизация системных часов'
 timedatectl set-ntp true
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-	g
-	n
-	;
-	;
-	+512M | y
-	t
-	1
-	n
-	;
-	;
-	+512M | y
-	n
-	;
-	;
-	; | y
-	t
-	3
-	31
-	w
-EOF
+echo 'Разметка дисков'
+(
+	echo g;
+	
+	echo n;
+	echo ;
+	echo ;
+	echo +512M;
+	echo t;
+	echo 1;
+	
+	
+	echo n;
+	echo ;
+	echo ;
+	echo +512M
+	
+	echo n;
+	echo ;
+	echo ;
+	echo ;
+	echo t;
+	echo 3;
+	echo 31;
+	echo w;
+	
+)	| fdisk /dev/sda
 
 echo 'Просмотр разделов'
 fdisk -l
