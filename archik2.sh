da5b24b3b843276fc116626d6df0f64628f9b226
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-25 22:26:53 +0300

    Update archik2.sh

--- archik2.sh
+++ archik2.sh
@@ -1,51 +1,13 @@
 #!/bin/sh
-echo 'hostname'
-echo "archik" > /etc/hostname
 
-echo 'hosts'
-echo "127.0.0.1	localhost" >> /etc/hosts
-echo "::1" >> /etc/hosts
-echo "127.0.1.1	archik.localdomain  archik" >> /etc/hosts
+echo 'Создаем нового пользователя'
+useradd -m -g users -G wheel,audio,disk,floppy,input,kvm,optical,scanner,storage,video,games,lp,power -s /bin/bash sergey
 
-echo 'Часовой пояс'
-ln -sf /usr/share/zoneinfo/Europe/Kiev /etc/localtime
+echo 'Пароль нового пользователя'
+echo "sergey:1998" | chpasswd
 
-echo 'Способ хранения таймера'
-hwclock --systohc --utc
+echo 'sudoers'
+sed -i 's/# wheel ALL=(ALL) ALL/wheel ALL=(ALL) ALL/' /etc/sudoers
 
-echo 'Добавляем локаль системы'
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-echo "ru_UA.UTF-8 UTF-8" >> /etc/locale.gen 
-
-echo 'Обновим текущую локаль системы'
-locale-gen
-
-echo 'Указываем язык системы'
-echo 'LANG="ru_UA.UTF-8"' > /etc/locale.conf
-
-echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
-echo 'KEYMAP=ru' >> /etc/vconsole.conf
-echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
-
-echo 'Создадим загрузочный RAM диск'
-mkinitcpio -p linux
-
-echo 'Устанавливаем загрузчик'
-pacman -S grub efibootmgr --noconfirm
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi
-grub-install --target=x86_64-efi --bootloader-id=Archik --efi-directory=/boot/efi --recheck
-
-echo 'Обновляем grub.cfg'
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo 'Ставим программу для Wi-fi'
-pacman -S dialog wpa_supplicant --noconfirm
-
-echo 'passwd'
-(
-    echo 1998;
-    echo 1998;
-)   | passwd 
-
-echo 'Установка завершена'
+echo ''
+sed -i 's/#[multilib]\n#Include = /etc/pacman.d/mirrorlist/[multilib]\nInclude = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
