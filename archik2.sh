1e9cc76a323554a0230f2d42f54e7ed3e92bbc6b
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-25 23:39:58 +0300

    Update archik2.sh

--- archik2.sh
+++ archik2.sh
@@ -7,7 +7,31 @@ echo 'Пароль нового пользователя'
 echo "sergey:1998" | chpasswd
 
 echo 'sudoers'
-sed -i 's/# wheel ALL=(ALL) ALL/wheel ALL=(ALL) ALL/' /etc/sudoers
+sed -i 's|^# wheel ALL=(ALL) ALL|wheel ALL=(ALL) ALL|' /etc/sudoers
 
-echo ''
-sed -i 's/#[multilib]\n#Include = /etc/pacman.d/mirrorlist/[multilib]\nInclude = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
+echo 'multilib'
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf 
+
+echo 'Обновление зеркалов'
+pacman -Syy
+
+echo 'Установка Xorg' 
+pacman -S xorg-server xorg-apps xorg-xinit
+
+echo 'Установка Драйверов'
+pacman -S xf86-video-intel lib32-intel-dri intel-ucode
+
+echo 'Установка оконного менеджера'
+pacman -S i3-wm i3lock 
+
+echo 'Установка экранного менеджера'
+pacman -S lightdm lightdm-gtk-greeter
+
+echo 'Установка директории пользователя'
+pacman -S xdg-user-dirs 
+
+echo 'Установка драйвера тачпада'
+pacman -S xf86-input-synaptics
+
+echo 'Установка звука'
+pacman -S alsa-lib alsa-utils alsa-oss 
