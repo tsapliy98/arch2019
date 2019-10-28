2ca08cf72221c87884988877bec4f3295012e85b
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-28 03:40:38 +0200

    Update archik4.sh

--- archik4.sh
+++ archik4.sh
@@ -3,91 +3,91 @@
 read -p "Выберите разметку диска: 1 MBR; 2 GPT_UEFI; 3 GPT_UEFI_LVM; " disk
 if [[ "$disk" -eq 1 ]]; then
     ( 
-        o
-        n
-        p
-        ;
-        ;
-        +512M
-            y
-        a
-        n
-        p
-        ;
-        ;
-        +2G
-            y
-        n
-        p
-        ;
-        ;
-        +20G
-            y
-        n
-        p
-        ;
-        ;
-        ;
-            y
-        t
-        2
-        82
-        w
+       echo o;
+       echo n;
+       echo p;
+       echo ;
+       echo ;
+       echo +512M;
+       echo         y;
+       echo a;
+       echo n;
+       echo p;
+       echo ;
+       echo ;
+       echo +2G;
+       echo         y;
+       echo n;
+       echo p;
+       echo ;
+       echo ;
+       echo +20G;
+       echo         y;
+       echo n;
+       echo p;
+       echo ;
+       echo ;
+       echo ;
+       echo         y;
+       echo t;
+       echo 2;
+       echo 82;
+       echo w;
     )   | fdisk /dev/sda
 elif [[ "$disk" -eq 2 ]]; then
     ( 
-        g
-        n
-        ;
-        ;
-        +512M
-            y
-        t
-        1
-        n
-        ;
-        ;
-        +2G
-            y
-        n
-        ;
-        ;
-        +20G
-            y
-        n
-        ;
-        ;
-        ;
-            y
-        t
-        2
-        19
-        w
+       echo g;
+       echo n;
+       echo ;
+       echo ;
+       echo +512M;
+       echo         y;
+       echo t;
+       echo 1;
+       echo n;
+       echo ;
+       echo ;
+       echo +2G;
+       echo         y;
+       echo n;
+       echo ;
+       echo ;
+       echo +20G;
+       echo         y;
+       echo n;
+       echo ;
+       echo ;
+       echo ;
+       echo         y;
+       echo t;
+       echo 2;
+       echo 19;
+       echo w;
     )   | fdisk /dev/sda
 elif [[ "$disk" -eq 3 ]]; then
     ( 
-        g
-        n
-        ;
-        ;
-        +512M
-            y
-        t
-        1
-        n
-        ;
-        ;
-        +512M
-            y
-        n
-        ;
-        ;
-        ;
-            y
-        t
-        3
-        31
-        w
+       echo g;
+       echo n;
+       echo ;
+       echo ;
+       echo +512M;
+       echo         y;
+       echo t;
+       echo 1;
+       echo n;
+       echo ;
+       echo ;
+       echo +512M;
+       echo         y;
+       echo n;
+       echo ;
+       echo ;
+       echo ;
+                    y;
+       echo t;
+       echo 3;
+       echo 31;
+       echo w;
    )    | fdisk /dev/sda
 fi 
 
