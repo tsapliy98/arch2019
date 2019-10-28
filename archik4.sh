28b79560fdd5077666fa4d3de403509ac9c35db6
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-28 03:06:21 +0200

    Update archik4.sh

--- archik4.sh
+++ archik4.sh
@@ -1,38 +1,95 @@
-#!/bin/sh
-echo 'Создание директорий'
-mkdir ~/Downloads
-mkdir ~/Documents
-mkdir ~/Pictures
-mkdir ~/Music
-mkdir ~/Video
-mkdir ~/.icons
-mkdir ~/.themes
+#!/bin/bash
 
-echo 'Установка программ'
-sudo pacman -S firefox firefox-i18n-ru qbittorrent feh gpicview vlc kdeconnect udiskie thunar p7zip tar file-roller unrar libreoffice-fresh libreoffice-fresh-ru libreoffice-fresh-uk atom evince  compton lxappearance 
-
-
-
-sudo pacman -Syyu 
-
-cd Downloads
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -si 
-cd ~
-
-echo 'Установка yay'
-cd Downloads
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si 
-cd ~
-
-yay -S ttf-ms-fonts ttf-tahoma ttf-vista-fonts pa-applet-git polybar timeshift  
-
-mkdir -p ~/.config/polybar
-cp /usr/share/doc/polybar/config ~/.config/polybar
-
+read -p "Выберите разметку диска: 1 MBR; 2 GPT_UEFI; 3 GPT_UEFI_LVM; " disk
+if [[ "$disk" -eq 1 ]]; then
+    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+        o
+        n
+        p
+        ;
+        ;
+        +512M
+            y
+        a
+        n
+        p
+        ;
+        ;
+        +2G
+            y
+        n
+        p
+        ;
+        ;
+        +20G
+            y
+        n
+        p
+        ;
+        ;
+        ;
+            y
+        t
+        2
+        82
+        w
+    EOF
+elif [[ "$disk" -eq 2 ]]; then
+    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+        g
+        n
+        ;
+        ;
+        +512M
+            y
+        t
+        1
+        n
+        ;
+        ;
+        +2G
+            y
+        n
+        ;
+        ;
+        +20G
+            y
+        n
+        ;
+        ;
+        ;
+            y
+        t
+        2
+        19
+        w
+    EOF
+elif [[ "$disk" -eq 3 ]]; then
+    sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+        g
+        n
+        ;
+        ;
+        +512M
+            y
+        t
+        1
+        n
+        ;
+        ;
+        +512M
+            y
+        n
+        ;
+        ;
+        ;
+            y
+        t
+        3
+        31
+        w
+    EOF
+fi 
 
 
 
